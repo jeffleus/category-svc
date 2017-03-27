4b2e27dd571af45b373cb2c58562e5ddd43fa324
jeffleus <jeffleus@gmail.com>
2017-03-27 13:28:28 -0700

    init category micro service
    using the serverless framework for a set of was micro services

---
+++ Category.js
@@ -0,0 +1,105 @@
+'use strict';
+var Sequelize = require('sequelize');
+var sequelize = new Sequelize('FuelStation_EDA', 'callsheetadmin', 'Eraser$17', {
+	host: 'callsheet-mysql.cn6x6nhayn9c.us-west-2.rds.amazonaws.com',
+	port: 3306,
+    pool: {
+        max: 10,
+        min: 1,
+        idle: 100
+    }
+});
+
+var Category = sequelize.define('category', {
+  CateogryID: { 
+	  type: Sequelize.INTEGER, 
+	  primaryKey: true, 
+      autoincrement: true, 
+	  field: 'CategoryID' 
+  }, 
+  name: { type: Sequelize.STRING, field: 'Name' }, 
+  description: { type: Sequelize.STRING, field: 'Description' }
+}, {
+	tableName: 'Categories'
+});
+
+var moduleName = "CATEGORY:";
+
+module.exports.get = function(id,filter) {
+    if (!id) return list(filter);
+    console.log(moduleName, 'calling getSingle with id: ' + id);
+    return sequelize.sync().then(function() {
+        return Category.findById(id).then(function(category) {
+            console.info(moduleName, 'category record found');
+            return {
+                count: (category)?1:0,
+                categories: [ (category)?category.dataValues:null ]
+            };
+        })
+    });
+}
+
+function list(filter) {
+    console.log(moduleName, 'calling getAll because no id provided');
+	return sequelize.sync().then(function() {
+        if (filter) {
+            var filterOption = {
+                where: {
+                    CategoryID: filter 
+                } 
+            };
+            return Category.findAndCountAll(filterOption);
+        } else return Category.findAndCountAll();
+    }).then(function(result) {
+		//return Athlete.findAndCountAll().then(function(result) {
+        var categories = [];
+        result.rows.forEach(function(categoryRow) {
+            categories.push(categoryRow.dataValues);
+        });
+        return {
+            count: result.count,
+            categories: categories
+        };
+	});
+}
+
+module.exports.create = function(json) {
+	return sequelize.sync().then(function() {
+		console.info(moduleName, 'create a new category using JSON provided');
+		console.error('need to add json validation to category creation');
+		var categoryJson = json;//JSON.parse(json);
+		return Category.create(json).then(function(category) {
+			console.info('category successfully created');
+			return category;
+		});
+	});
+};
+
+module.exports.update = function(json) {
+	return sequelize.sync().then(function() {
+		console.info(moduleName, 'update a single category using JSON provided');
+		console.error('need to add json validation to category update');
+		var ath = json;//JSON.parse(json);
+		return Category.update(
+			json,
+			{ where: { CategoryID: json.CategoryID } }
+		).then(function(result) {
+			console.info(moduleName, 'category successfully updated');
+			return result;
+		});
+	});
+};
+
+module.exports.delete = function(id) {
+	return sequelize.sync().then(function() {
+		console.info(moduleName, 'delete a category by id');
+		return Category.destroy({ where: { CategoryID: id } }).then(function(count) {
+			console.info(moduleName, '(' + count.toString() + ') categories successfully deleted');
+			return count;
+		});
+	});
+};
+
+module.exports.close = function() {
+	sequelize.close();
+};
---
+++ index.js
@@ -0,0 +1,45 @@
+var Category = require('./Category');
+
+var args = process.argv.slice(2);
+//console.log(process.argv);
+
+var sport = {
+	SportCodeID: 'XXX', 
+	description: "yo, dis an update suckah",
+	inSeasonStart: "2016-11-01T12:12:12.123Z",
+	inSeasonEnd: "2017-03-24T12:12:12.123Z",
+	offSeasonStart: "2017-03-25T12:12:12.123Z",
+	offSeasonEnd: "2017-06-30T12:12:12.123Z"
+};
+
+var id = 1;
+var filter = ('1,3,5,7').split(',');
+console.log("FILTER: ", filter);
+
+Category.get(null, null).then(function(student) {
+
+//Sport.get(null, filter).then(function(student) {
+//Sport.get('XXX').then(function(student) {
+//Sport.create(sport).then(function(student) {
+//Sport.update(sport).then(function(student) {
+//Sport.delete('XXX').then(function(result) {
+	
+//Athletes.delete(1165).then(function(result) {
+//Athletes.update(ath).then(function(student) {
+//Athletes.create(ath).then(function(student) {
+//Athletes.get(id, filter).then(function(student) {
+//	console.log(result);
+	console.log(student);
+	return;
+}).catch(function(err) {
+	console.error(err);
+	return;
+}).finally(function() {
+	Category.close();
+	return;
+});
+
+function _logTest(id, filter) {
+    console.log('ID: ', id);
+    console.log('FILTER: ', filter);
+};
